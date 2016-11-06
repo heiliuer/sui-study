77c24a5530eb0c0eaf6d718ac3a10376266c0693
heiliuer <eee>
2016-11-06 13:35:30 +0800

    Initial commit

---
+++ 002title-bar/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Heiliuer on 2016/11/6 0006.
+ */
+$.init();
+
+$("[data-action-left-panel]").click(function () {
+    $.openPanel();
+})
---
+++ reademe.md
@@ -0,0 +1,3 @@
+### sui-study
+
+- [参考文档](http://m.sui.taobao.org/components/#init)
